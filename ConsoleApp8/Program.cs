97b950db1cca46b9a771d9ab45a51c3029625073
welshboy54 <37490748+welshboy54@users.noreply.github.com>
2019-11-01 15:15:27 -0500

    Constructors and Destructors

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-namespace ConsoleApp8
+namespace ClassesTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car;
+
+            car = new Car("Red");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("Green");
+            Console.WriteLine(car.Describe());
+
+            car = new Car();
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Car
+    {
+        private string color;
+
+        public Car()
+        {
+            Console.WriteLine("Constructor with no paremeter called");
+        }
+
+        public Car(string color)
+        {
+            this.color = color;
+        }
+
+        public string Describe()
+        {
+            return "This car is " + Color;
+        }
+
+        public string Color
+        {
+            
+            get
+            {
+                return color.ToUpper();
+            }
+            set
+            {
+                if (value == "Red")
+                {
+                    color = value;
+                }
+                else
+                    Console.WriteLine("this car can only be red!");
+            }
         }
     }
 }
